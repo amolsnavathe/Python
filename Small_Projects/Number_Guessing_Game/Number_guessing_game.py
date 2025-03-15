6ed012802010a416e03f418ab39f6a5e8ca80c73
Amol Navathe <asnavath@ncsu.edu>
2025-03-15 15:51:15 -0400

    Adding files

---
+++ Small_Projects/Number_Guessing_Game/Number_guessing_game.py
@@ -0,0 +1,19 @@
+import random
+
+number_to_guess = random.randint(1,100)
+playing = True
+
+while (playing):
+    try:
+        user_input = int(input("Guess the number between 1 and 100: "))
+
+        if (user_input < number_to_guess):
+            print("Too low")
+        if (user_input > number_to_guess):
+            print("Too high")
+        elif (user_input == number_to_guess):
+            print("\nCongratulations, you found the number (" + str(number_to_guess) + ")")
+            playing = False
+            break
+    except ValueError:
+        print("Please enter a valid number")
